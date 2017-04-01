8937ab558df00fb717d432802bf9061be629a5f9
Arman Yaraee <arman@yaraee.net>
2017-04-01 10:08:15 +0100

    Updated aliases for better git usability

--- cmder/user-aliases.cmd
+++ cmder/user-aliases.cmd
@@ -4,6 +4,8 @@ pwd=cd
 clear=cls
 history=cat %CMDER_ROOT%\config\.history
 unalias=alias /d $1
-gl=git log --decorate --oneline --graph  
-gs=git status -s -b  
 bfg=java -jar C:\Libs\Git\BFG\bfg-1.12.13.jar $*  
+gs=git status -s -b $*
+gd=git diff $*  
+gl=git log --graph --pretty=format:"%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset" --abbrev-commit $*
+glm=git log --graph --pretty=format:"%Cred%h%Creset - %s %Cgreen(%cr) %C(bold blue)<%an>%Creset" --abbrev-commit $*
