26b51c858d293ecae477679bb312eedee060271a
Arman <ayaraee1@gmail.com>
2022-01-22 01:25:15 +0000

    Update submodules

--- install.arijoon.sh
+++ install.arijoon.sh
@@ -1,9 +1,13 @@
-sudo apt-get install zsh git -y
+sudo apt-get install zsh git tmux -y
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone --recursive https://github.com/arijoon/Dotfiles.git ~/.dotfiles
 rm ~/.zshrc
 ln -s ~/.dotfiles/.vim/ ~/.vim
 ln -s ~/.dotfiles/.vim/.vimrc ~/.vimrc
 ln -s ~/.dotfiles/.zsh/.zshrc ~/.zshrc
+ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/.dotfiles/.zsh/agnoster1.zsh-theme ~/.oh-my-zsh/themes/agnoster1.zsh-theme 
 ln -s ~/.dotfiles/.zsh/powerlevel9k ~/.oh-my-zsh/custom/themes/powerlevel9k
+
+# Install tmux plugin manager
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
